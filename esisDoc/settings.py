fac07e6036f5d5a1ab4334a8c18ccdd4088c1f26
belange-mulamba <19km166@esisalama.org>
2023-09-20 10:38:09 +0200

    projet tuttore v2

--- esisDoc/settings.py
+++ esisDoc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xfb*!gv2dm#lrzm+(g2l*9g!n&rlg2_db!j#h&%fq(#(zd=4@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
